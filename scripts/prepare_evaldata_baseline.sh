99123d64deceb5d12ac20a665dfea25faf88e118
Xaver <xaver.97@hotmail.de>
2024-05-29 15:37:34 +0200

    baseline eval data script

--- scripts/prepare_evaldata_baseline.sh
+++ scripts/prepare_evaldata_baseline.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-mkdir -p dialects_eval_data
-cd dialects_eval_data
+mkdir -p baseline_eval_data
+cd baseline_eval_data
 
-# copy dialects eval data together:
+# copy baseline eval data together:
 # adapt absolute paths for out of colab cases!
 
-cp /content/BaySIDshot/de-by.test.conll .
-cp /content/BaySIDshot/de-by.dev.conll .
-
-cp /content/BaySIDshot/xsid/data/xSID-0.5/de-ba.test.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.5/de-ba.valid.conll .
-
-python3 /content/BaySIDshot/scripts/reorganize_miriams_data.py de-ba.test.conll
-python3 /content/BaySIDshot/scripts/reorganize_miriams_data.py de-ba.valid.conll
-
-cp /content/BaySIDshot/xsid/data/xSID-0.4/de.test.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/de.valid.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/de-st.test.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/de-st.valid.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/en.test.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/en.valid.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/gsw.test.conll .
-cp /content/BaySIDshot/xsid/data/xSID-0.4/gsw.valid.conll .
+cp /content/BaySIDshot/xsid/data/xSID/*.test.conll .
+cp /content/BaySIDshot/xsid/data/xSID/*.valid.conll .
 
 cd ..
