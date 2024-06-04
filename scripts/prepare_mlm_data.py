4aff56fdfe18d20cdaf0416e8ff868a592edf1f4
Xaver <xaver.97@hotmail.de>
2024-06-04 16:24:41 +0200

    added Bavarian MLM data from dialect-BLI data

--- scripts/prepare_mlm_data.py
+++ scripts/prepare_mlm_data.py
@@ -36,19 +36,31 @@ def write_to_file(sentences, path):
     try:
         with open(path, 'w', encoding='utf-8') as file:
             file.write(''.join(sentences))
-        print(f"Bavarian MLM Data successfully written to {path}")
+        print(f"{len(sentences)} Bavarian MLM Data successfully written to {path}")
     except Exception as e:
         print(f"Error writing Bavarian MLM Data to file: {e}")
 def main():
     path_to_bar = '../dialect-BLI/labelled_data/bitext/bar/ann_1.csv'
-    bar_save_path = "../manual_data/MLM_data/mlm_data_bar_ann_1.txt"
+    #test_save_path = "../manual_data/MLM_data/mlm_bar_ann_1_test.txt"
+    train_save_path = "../manual_data/MLM_data/mlm_bar_ann_1_train.txt"
+    dev_save_path = "../manual_data/MLM_data/mlm_bar_ann_1_dev.txt"
 
     data = read_data(path_to_bar)
     if data is None:
         return
 
-    write_to_file(sort_sentences(data), bar_save_path)
+    #write_to_file(sort_sentences(data), test_save_path)
 
+    sorted_sentences = sort_sentences(data)
+    total_sentences = len(sorted_sentences)
+
+    # Splitting into train and dev data
+    train_size = int(0.9 * total_sentences)
+    train_sentences = sorted_sentences[:train_size]
+    dev_sentences = sorted_sentences[train_size:]
+
+    write_to_file(train_sentences, train_save_path)
+    write_to_file(dev_sentences, dev_save_path)
 
 if __name__ == "__main__":
     main()
