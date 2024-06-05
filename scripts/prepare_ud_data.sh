0fee34590758986cbd943fd7f4fd747086b970bd
Xaver <xaver.97@hotmail.de>
2024-06-05 09:28:36 +0200

    adopting prepare UD data

--- scripts/prepare_ud_data.sh
+++ scripts/prepare_ud_data.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 # script to prepare the UD_Bavarian-MaiBaam data to be used with MaChAmp and as train and dev 90-10 split
-mkdir -p UD_data
-cd UD_data
-cp /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu .
-python3 /content/BaySIDshot/machamp/scripts/misc/cleanconl.py /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu
-python3 /content/BaySIDshot/scripts/testsplit_conllu.py bar_maibaam-ud-test.conllu
-cd ..
+mkdir -p /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data
+#cd UD_data
+#cp /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu .
+#python3 /content/BaySIDshot/machamp/scripts/misc/cleanconl.py /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu
+#python3 /content/BaySIDshot/scripts/testsplit_conllu.py bar_maibaam-ud-test.conllu
+cp /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data
+python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/machamp/scripts/misc/cleanconl.py #/Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data/bar_maibaam-ud-test.conllu
+python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/scripts/testsplit_conllu.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data_bar_maibaam-ud-test.conllu
+#cd ..
