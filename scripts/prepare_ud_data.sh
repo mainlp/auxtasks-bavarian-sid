ecf485778050e0c43a219696549c164c14f2def2
Xaver <xaver.97@hotmail.de>
2024-06-05 09:55:08 +0200

    fixed prepare UD data and added new manually created UD data

--- scripts/prepare_ud_data.sh
+++ scripts/prepare_ud_data.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # script to prepare the UD_Bavarian-MaiBaam data to be used with MaChAmp and as train and dev 90-10 split
-mkdir -p /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data
-#cd UD_data
-#cp /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu .
-#python3 /content/BaySIDshot/machamp/scripts/misc/cleanconl.py /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu
-#python3 /content/BaySIDshot/scripts/testsplit_conllu.py bar_maibaam-ud-test.conllu
-cp /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data
-python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/machamp/scripts/misc/cleanconl.py #/Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data/bar_maibaam-ud-test.conllu
-python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/scripts/testsplit_conllu.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data_bar_maibaam-ud-test.conllu
-#cd ..
+# command from BaySIDshot:  bash scripts/prepare_ud_data.sh
+
+# in Colab
+mkdir -p /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/UD_data
+cp /content/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu /content/BaySIDshot/UD_data
+python3 /content/BaySIDshot/machamp/scripts/misc/cleanconl.py /content/BaySIDshot/UD_data/bar_maibaam-ud-test.conllu
+python3 /content/BaySIDshot/scripts/testsplit_conllu.py /content/BaySIDshot/UD_data/bar_maibaam-ud-test.conllu
+
+# on loacal machine
+#mkdir -p /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data
+#cp /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/UD_Bavarian-MaiBaam/bar_maibaam-ud-test.conllu /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data
+#python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/machamp/scripts/misc/cleanconl.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data/bar_maibaam-ud-test.conllu
+#python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/scripts/testsplit_conllu.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/UD_data/bar_maibaam-ud-test.conllu
--- scripts/testsplit_conllu.py
+++ scripts/testsplit_conllu.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 import sys
+import os
 import random
 
-# script to train-dev split bar_maibaam-ud-test.conllu data with a 90-10 split for aux task training
+
+# script to train-dev split cleaned bar_maibaam-ud-test.conllu data with a 90-10 split for aux task training
 
 def read_conllu_file(filename):
     with open(filename, 'r', encoding='utf-8') as f:
@@ -15,18 +17,18 @@ def write_conllu_file(filename, data):
     with open(filename, 'w', encoding='utf-8') as f:
         count = 0
         for example in data:
-            count+=1
+            count += 1
             f.write(example + '\n\n')
         print("Wrote ", count, "files to ", filename)
 
 
 def filter_examples(data):
     print("All examples: ", len(data))
-    # simply filtering for examples that start with # sent_id = xsid 
+    # simply filtering for examples that start with # sent_id = xsid
     # come from xSID data and could mess up SID task results there!
     filtered = [example for example in data if not example.startswith('# sent_id = xsid')]
     print("Filtered examples: ", len(filtered))
-    
+
     return filtered
 
 
@@ -39,16 +41,21 @@ def split_data(data, train_ratio=0.9):
 def main(input_filename):
     data = read_conllu_file(input_filename)
     filtered_data = filter_examples(data)
-
     train_data, valid_data = split_data(filtered_data)
 
-    write_conllu_file('bar_maibaam-ud-train.conllu', train_data)
-    write_conllu_file('bar_maibaam-ud-valid.conllu', valid_data)
+    output_dir = os.path.dirname(input_filename)
+    base_name = os.path.splitext(os.path.basename(input_filename))[0]
+
+    train_filename = os.path.join(output_dir, f"{base_name}-train.conllu")
+    valid_filename = os.path.join(output_dir, f"{base_name}-valid.conllu")
+
+    write_conllu_file(train_filename, train_data)
+    write_conllu_file(valid_filename, valid_data)
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: ! python3 testsplit_conllu.py <input_filename>")
+        print("Usage: python3 testsplit_conllu.py <input_filename>")
         sys.exit(1)
 
     input_filename = sys.argv[1]
