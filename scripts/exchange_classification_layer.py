be0170cfd93695b6ac7a96ffb06674dc1d3a2efd
Xaver <xaver.97@hotmail.de>
2024-06-11 16:52:37 +0200

    tries to exchange classification layer

--- scripts/exchange_classification_layer.py
+++ scripts/exchange_classification_layer.py
@@ -6,7 +6,7 @@ import torch
 def load_model(filepath):
     return torch.load(filepath, map_location=torch.device('cuda'))
 
-def replace_classification_head(model0_path, model1_path, model_out_path):
+def replace_classification_layer(model0_path, model1_path, model_out_path):
     # Load the first model and extract its classification layer
     model0 = load_model(model0_path)
     classification_layer = model0.classifier
@@ -22,8 +22,8 @@ def replace_classification_head(model0_path, model1_path, model_out_path):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python3 exchange_classification_heads.py <model0_path> <model1_path> <model_out_path>")
+        print("Usage: python3 exchange_classification_layer.py <model0_path> <model1_path> <model_out_path>")
         sys.exit(1)
 
-    replace_classification_head(sys.argv[1], sys.argv[2], sys.argv[3])
+    replace_classification_layer(sys.argv[1], sys.argv[2], sys.argv[3])
     print("Successfully replaced the classification layer and saved the modified second model.")
