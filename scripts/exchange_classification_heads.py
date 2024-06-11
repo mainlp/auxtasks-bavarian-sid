e95797389f53ab3e8963288bf482c45a8c1c2338
Xaver <xaver.97@hotmail.de>
2024-06-11 16:44:39 +0200

    small path fixes

--- scripts/exchange_classification_heads.py
+++ scripts/exchange_classification_heads.py
@@ -2,35 +2,28 @@
 
 import sys
 import torch
-from transformers import AutoModelForSequenceClassification
 
-if __name__ == "__main__":
+def load_model(filepath):
+    return torch.load(filepath, map_location=torch.device('cuda'))
 
-    if len(sys.argv) != 4:
-        print("Usage: python3 exchange_classification_heads.py <model0_path> <model1_path>, <model_out_path>")
-        sys.exit(1)
-
+def replace_classification_head(model0_path, model1_path, model_out_path):
     # Load the first model and extract its classification layer
-    #model0_path = '/content/drive/MyDrive/Masterarbeit/test_exchange_heads.0/*/model_*'
-    model0_path = sys.argv[1]
-    model0 = AutoModelForSequenceClassification.from_pretrained('microsoft/mdeberta-v3-base')
-    model0.load_state_dict(torch.load(model0_path))
-
-    # Extract the classification layer from the first model
+    model0 = load_model(model0_path)
     classification_layer = model0.classifier
 
     # Load the second model
-    #model1_path = '/content/drive/MyDrive/Masterarbeit/test_exchange_heads.1/*/model_*'
-    model1_path = sys.argv[2]
-    model1 = AutoModelForSequenceClassification.from_pretrained('microsoft/mdeberta-v3-base')
-    model1.load_state_dict(torch.load(model1_path))
+    model1 = load_model(model1_path)
 
     # Set the classification layer of the first model as the classification layer in the second model
     model1.classifier = classification_layer
 
     # Save the modified second model
-    #model_out_path = '/content/drive/MyDrive/Masterarbeit/'
-    model_out_path = sys.argv[3]
-    torch.save(model1.state_dict(), model_out_path)
+    torch.save(model1, model_out_path)
 
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: python3 exchange_classification_heads.py <model0_path> <model1_path> <model_out_path>")
+        sys.exit(1)
+
+    replace_classification_head(sys.argv[1], sys.argv[2], sys.argv[3])
     print("Successfully replaced the classification layer and saved the modified second model.")
