95996d09eaa6adea2c5c5bc118c8472505892a4d
Xaver <xaver.97@hotmail.de>
2024-07-01 09:51:54 +0200

    fixed paths

--- scripts/prepare_evaldata_natural.sh
+++ scripts/prepare_evaldata_natural.sh
@@ -8,8 +8,11 @@ mkdir -p /content/BaySIDshot/natural_eval_data
 cp /content/BaySIDshot/NaLiBaSID/data/data.zip /content/BaySIDshot/natural_eval_data/
 unzip -P MaiNLP -o /content/BaySIDshot/natural_eval_data/data.zip -d /content/BaySIDshot/natural_eval_data/
 mv -f /content/BaySIDshot/natural_eval_data/de-ba.MAS.test.conll /content/BaySIDshot/natural_eval_data/de-ba-MAS.test.conll
+python3 /content/BaySIDshot/scripts/reorganize_natural_data.py /content/BaySIDshot/natural_eval_data/de-ba-MAS.test.conll
 mv -f /content/BaySIDshot/natural_eval_data/de-ba.xMAS.test.conll /content/BaySIDshot/natural_eval_data/de-ba-xMAS.test.conll
+python3 /content/BaySIDshot/scripts/reorganize_natural_data.py /content/BaySIDshot/natural_eval_data/de-ba-xMAS.test.conll
 mv -f /content/BaySIDshot/natural_eval_data/de-ba.nat.conll /content/BaySIDshot/natural_eval_data/de-ba-nat.test.conll
+python3 /content/BaySIDshot/scripts/reorganize_natural_data.py /content/BaySIDshot/natural_eval_data/de-ba-nat.test.conll
 rm -f /content/BaySIDshot/natural_eval_data/de-ba.MAS.valid.conll
 rm -f /content/BaySIDshot/natural_eval_data/de-ba.xMAS.valid.conll
 rm -f /content/BaySIDshot/natural_eval_data/de-ba.valid.conll
@@ -23,5 +26,18 @@ rm -f /content/BaySIDshot/natural_eval_data/data.zip
 # on local machine - do not push this - data should stay "private"!
 #mkdir -p /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data
 #cp /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/NaLiBaSID/data/data.zip /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/
-#unzip -P MaiNLP /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/Raw_data/data.zip -d /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/
+#unzip -P MaiNLP -o /content/BaySIDshot/natural_eval_data/data.zip -d /content/BaySIDshot/natural_eval_data/
+#mv -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.MAS.test.conll /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba-MAS.test.conll
+#python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/scripts/reorganize_natural_data.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba-MAS.test.conll
+#mv -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.xMAS.test.conll /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba-xMAS.test.conll
+#python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/scripts/reorganize_natural_data.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba-xMAS.test.conll
+#mv -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.nat.conll /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba-nat.test.conll
+#python3 /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/scripts/reorganize_natural_data.py /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba-nat.test.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.MAS.valid.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.xMAS.valid.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.valid.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/de-ba.test.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/lt.nat.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/lt.test.conll
+#rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/lt.valid.conll
 #rm -f /Users/xavermariakrueckl/PycharmProjects/BaySIDshot/manual_data/natural_eval_data/data.zip
